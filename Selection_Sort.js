86d7be7eda31147332196d0b94b12eb76cd88cf8
harshbhatt2106 <harshbhatt932@gmail.com>
2025-01-08 21:25:21 +0530

    Selection_Sort

--- Selection_Sort.js
+++ Selection_Sort.js
@@ -1,5 +1,19 @@
-for (let index = 0; index < array.length; index++) 
+const array = [4,8,6,2,5,7]
+let temp;
+for (let i = 0; i < array.length-1; i++) 
 {
-    const element = array[index];
-    
+    let  Small_Index = i;
+    for (let j = i+1; j < array.length; j++) 
+    {
+        if (array[j]<array[Small_Index])
+        {
+            Small_Index = j;    
+        }   
+    }   
+    temp = array[i];
+    array[i] = array[Small_Index]
+    array[Small_Index] = temp; 
 }
+array.forEach(data=>{
+    console.log(data);
+})
