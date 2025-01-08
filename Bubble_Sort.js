21f92b2f370686927bf5917e9929d29ebda25918
harshbhatt2106 <harshbhatt932@gmail.com>
2025-01-08 20:51:04 +0530

    Sorting_Tchnique

---
+++ Bubble_Sort.js
@@ -0,0 +1,24 @@
+//--------------Bubble sort-----------------------
+const array = [1,2,3,4,5];
+let temp;
+let IsSorted = false;
+for (let i = 0; i < array.length-1; i++)
+{
+    for (let j = 0; j < array.length-i-1; j++)
+    {
+        if (array[j]>array[j+1])
+        {
+            temp = a[j];
+            a[j] = a[j+1];
+            a[j+1] = temp;
+            IsSorted = true;
+        }    
+    }    
+    if (!IsSorted)
+    {
+        break;
+    }
+}
+array.forEach(data=>{
+    console.log(data);
+})
